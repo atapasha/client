d87bd42b21041af69bc81be2bb3eb470f58e7a48
atapasha <atapashazade@gmail.com>
2023-10-27 10:35:44 +0330

    authentication

---
+++ src/scenes/layout/index.jsx
@@ -0,0 +1,35 @@
+ import {React,useState} from 'react';
+ import {Box, useMediaQuery} from '@mui/material'
+ import {Outlet} from 'react-router-dom'
+ import { useSelector } from 'react-redux';
+import Navbar from '../../components/Navbar';
+import Sidebar from '../../components/Sidebar'
+const Layout = () => {
+
+  const isNonMobile=useMediaQuery("(min-width:600px)")
+
+  const [isSidebarOpen,setisSidebarOpen]=useState(true)
+
+
+  return (
+    
+    <Box  display={isNonMobile?"flex":"block"} width="100%" height="100%">
+<Sidebar isNonMobile={isNonMobile} drawidth="250px"  isSidebarOpen={isSidebarOpen} setisSidebarOpen={setisSidebarOpen}  />
+
+
+<Box>
+  <Navbar
+   isSidebarOpen={isSidebarOpen} 
+   setisSidebarOpen={setisSidebarOpen}
+  />  
+  <Outlet />
+</Box>
+
+
+    </Box>
+
+
+  )
+}
+
+export default Layout
